12f953a40075edc46a22ef0f81bcad2d26939f7e
Victor Jafet <pl248111@ahschool.com>
2023-02-17 17:12:40 -0500

    time to debug for a million years

--- app.py
+++ app.py
@@ -1,21 +1,53 @@
-from flask import Flask, render_template, Response, request #flask imports
-from robotLibrary import Robot #import custom made robotLib class for abstraction
-from camera import CameraStream #imports "camerastream" class from 'camera.py' file located in the same dir as this app.py file
-import cv2 #opencv import
-import time
-
-import logging #temp
-
-
-app = Flask(__name__) #initialize flask object instance
-robot = Robot() #initialize robot class instance
-cap = CameraStream().start() #initialize camerastream object instance from other file
-
 #flask run --host=0.0.0.0
 
-#old code, would stream terminal output into logFile
-#logging.basicConfig(filename='static/logFile.txt', level=logging.DEBUG, filemode = 'w')
 
+#regular flask imports
+from flask import Flask, render_template, Response, request, redirect, url_for
+
+#below imports are for the authentication system
+from flask_sqlalchemy import SQLAlchemy #allows interaction with the sqlite database
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user #allows for user authentication and session management
+from flask_wtf import FlaskForm #allows for the creation of forms that will import user information into the database
+from wtforms import StringField, PasswordField, SubmitField #allows for the creation of forms that will import user information into the database
+from wtforms.validators import InputRequired, Length, ValidationError #allows for the validation of user input in user registration/login forms
+from flask_bcrypt import Bcrypt #allows for the hashing of passwords
+
+#below import is custom made robotLib class for abstraction
+try:
+    from robotLibrary import Robot
+except:
+    print("robot not connected")
+
+#below imports are for camera
+from camera import CameraStream #imports "camerastream" class from 'camera.py' file located in the same dir as this app.py file
+import cv2 #opencv import
+
+
+import time
+
+
+
+#object initializations
+app = Flask(__name__) #initialize flask app object instance
+
+try:
+    robot = Robot() #initialize robot class instance
+except:
+    print("robot not connected")
+
+cap = CameraStream().start() #initialize camerastream object instance from camera.py file
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' #connects the sqlalchemy app to the database file
+db = SQLAlchemy(app) #initialize database object instance
+app.config['SECRET_KEY'] = 'thisisasecretkey' #used for the encryption of the session cookie
+
+bcrypt = Bcrypt(app)
+
+class User(db.Model, UserMixin): #creates user table in the database
+    id = db.Column(db.Integer, primary_key=True) #creates id column, unique identifier for each user
+    username = db.Column(db.String(20), nullable=False, unique=True) #username column, limits length to 20 characters and cannot be empty
+    password = db.Column(db.String(80), nullable=False) #password column, limits length to 80 characters once hashed and cannot be empty
 
 
 
@@ -40,7 +72,10 @@ def forward():
     timeMS = int(request.args.get('timeMS', default = 1000))
 
     log(str(request.environ['REMOTE_ADDR']) + " - " + time.strftime("%H:%M:%S", time.localtime()) + " | Forward\n")
-    robot.motorForward(speedL, speedR, timeMS)
+    try:
+        robot.motorForward(speedL, speedR, timeMS)
+    except:
+        print("robot not connected")    
     return "<p>forward</p>"
 
 @app.route("/backward", methods = ['GET','POST'])
@@ -50,7 +85,10 @@ def backward():
     timeMS = int(request.args.get('timeMS', default = 1000))
 
     log(str(request.environ['REMOTE_ADDR']) + " - " + time.strftime("%H:%M:%S", time.localtime()) + " | Backward\n")
-    robot.motorBackward(speedL, speedR, timeMS)
+    try:
+        robot.motorBackward(speedL, speedR, timeMS)
+    except:
+        print("robot not connected")    
     return "<p>backward</p>"
 
 @app.route("/left", methods = ['GET','POST'])
@@ -60,7 +98,10 @@ def left():
     timeMS = int(request.args.get('timeMS', default = 850))
 
     log(str(request.environ['REMOTE_ADDR']) + " - " + time.strftime("%H:%M:%S", time.localtime()) + " | Left\n")
-    robot.motorLeft(speedL, speedR, timeMS)
+    try:
+        robot.motorLeft(speedL, speedR, timeMS)
+    except:
+        print("robot not connected")    
     return "<p>left</p>"
 
 @app.route("/right", methods = ['GET','POST'])
@@ -70,7 +111,10 @@ def right():
     timeMS = int(request.args.get('timeMS', default = 850))
 
     log(str(request.environ['REMOTE_ADDR']) + " - " + time.strftime("%H:%M:%S", time.localtime()) + " | Right\n")
-    robot.motorRight(speedL, speedR, timeMS)
+    try:
+        robot.motorRight(speedL, speedR, timeMS)
+    except:
+        print("robot not connected")    
     return "<p>right</p>"
 
 
@@ -81,31 +125,46 @@ def right():
 @app.route("/stop", methods = ['GET','POST'])
 def stop():
     log(str(request.environ['REMOTE_ADDR']) + " - " + time.strftime("%H:%M:%S", time.localtime()) + " | Stop\n")
-    robot.motorStop()
+    try:
+        robot.motorStop()
+    except:
+        print("robot not connected")        
     return "<p>stop</p>"
 
 @app.route("/forwardUndef", methods = ['GET','POST'])
 def forwardUndef():
     log(str(request.environ['REMOTE_ADDR']) + " - " + time.strftime("%H:%M:%S", time.localtime()) + " | Forward Undef\n")
-    robot.motorForwardUndef()
+    try:
+        robot.motorForwardUndef()
+    except:
+        print("robot not connected")        
     return "<p>forwardundef</p>"
 
 @app.route("/backwardUndef", methods = ['GET','POST'])
 def backwardUndef():
     log(str(request.environ['REMOTE_ADDR']) + " - " + time.strftime("%H:%M:%S", time.localtime()) + " | Backward Undef\n")
-    robot.motorBackwardUndef()
+    try:
+        robot.motorBackwardUndef()
+    except:
+        print("robot not connected")        
     return "<p>backwardundef</p>"
 
 @app.route("/leftUndef", methods = ['GET','POST'])
 def leftUndef():
     log(str(request.environ['REMOTE_ADDR']) + " - " + time.strftime("%H:%M:%S", time.localtime()) + " | Left Undef\n")
-    robot.motorLeftUndef()
+    try:
+        robot.motorLeftUndef()
+    except:
+        print("robot not connected")        
     return "<p>leftundef</p>"
 
 @app.route("/rightUndef", methods = ['GET','POST'])
 def rightUndef():
     log(str(request.environ['REMOTE_ADDR']) + " - " + time.strftime("%H:%M:%S", time.localtime()) + " | Right Undef\n")
-    robot.motorRightUndef()
+    try:
+        robot.motorRightUndef()
+    except:
+        print("robot not connected")        
     return "<p>rightundef</p>"
 
 
