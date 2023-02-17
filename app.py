81fda26037018d3ce8e231158bbab940e264c78f
Victor Jafet <pl248111@ahschool.com>
2023-02-17 17:16:33 -0500

    stupid vscode manual save

--- app.py
+++ app.py
@@ -29,25 +29,17 @@ import time
 
 #object initializations
 app = Flask(__name__) #initialize flask app object instance
-
+cap = CameraStream().start() #initialize camerastream object instance from camera.py file
 try:
     robot = Robot() #initialize robot class instance
 except:
     print("robot not connected")
 
-cap = CameraStream().start() #initialize camerastream object instance from camera.py file
-
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' #connects the sqlalchemy app to the database file
 db = SQLAlchemy(app) #initialize database object instance
+bcrypt = Bcrypt(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' #connects the sqlalchemy app to the database file
 app.config['SECRET_KEY'] = 'thisisasecretkey' #used for the encryption of the session cookie
 
-bcrypt = Bcrypt(app)
-
-class User(db.Model, UserMixin): #creates user table in the database
-    id = db.Column(db.Integer, primary_key=True) #creates id column, unique identifier for each user
-    username = db.Column(db.String(20), nullable=False, unique=True) #username column, limits length to 20 characters and cannot be empty
-    password = db.Column(db.String(80), nullable=False) #password column, limits length to 80 characters once hashed and cannot be empty
 
 
 
@@ -59,6 +51,109 @@ with open("static/logFile.txt",'w') as logFile:
 def log(logItem):
     with open("static/logFile.txt", "at") as logFile: #'at' stands for "append text file"
         logFile.write(str(logItem))
+
+
+
+
+
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin): #creates user table in the database
+    id = db.Column(db.Integer, primary_key=True) #creates id column, unique identifier for each user
+    username = db.Column(db.String(20), nullable=False, unique=True) #username column, limits length to 20 characters and cannot be empty
+    password = db.Column(db.String(80), nullable=False) #password column, limits length to 80 characters once hashed and cannot be empty
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(
+            username=username.data).first()
+        if existing_user_username:
+            raise ValidationError(
+                'That username already exists. Please choose a different one.')
+
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Login')
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
+    return render_template('login.html', form=form)
+
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@ app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
+
+
+
+
+
+
+
+
+#-------------
+
+
 
 
 
@@ -172,9 +267,9 @@ def rightUndef():
 
 #below is camera related endpoints and functions
 
-@app.route('/') #main page route
-def index(): #main page function
-    return render_template('index.html')
+# @app.route('/') #main page route
+# def index(): #main page function
+#     return render_template('index.html')
 
 def gen_frame(): #generator function, meaning it runs like over and over again and the yield statement at the end instead of being a return it returns a ton over and over looped
     while cap: #maybe equivalent to while true? i mean its just the class instance so idk
